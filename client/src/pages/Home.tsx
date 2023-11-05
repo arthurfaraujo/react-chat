19068738a27e6bdd6f8e0ad58067d1e3af1056aa
arthur <arthurfelipe5567@gmail.com>
2023-11-04 23:51:11 -0300

    feat: create Home pages
    it

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <>
+      <div className="home">
+        <h1>React Chat</h1>
+        <p>React Chat is a simple chat that uses React and Socket.io.</p>
+      </div>
+    </>
+  )
+}
+
+export default Home
