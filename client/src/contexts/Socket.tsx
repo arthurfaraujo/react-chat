c07e9ccc01b8ab292071352104db3131ce902aad
arthurfaraujo <arthurfelipe5567@gmail.com>
2023-11-06 08:59:15 -0300

    fix: add a server url import
    it enables other endpoints to access the
    s

--- client/src/contexts/Socket.tsx
+++ client/src/contexts/Socket.tsx
@@ -2,6 +2,9 @@ import { createContext, ReactNode, useEffect, useState } from 'react'
 import { io, Socket } from 'socket.io-client'
 import { SocketContextIfc } from '../interfaces/SocketContextIfc'
 
+const server = import.meta.env.VITE_SERVER_URL || 'http://localhost:8080'
+console.log(server)
+
 export const SocketContext = createContext<SocketContextIfc>(
   {} as SocketContextIfc
 )
@@ -9,10 +12,10 @@ export const SocketContext = createContext<SocketContextIfc>(
 export const SocketProvider = ({ children }: { children: ReactNode }) => {
   const username = localStorage.getItem('user') || ''
   const [user, setUser] = useState(username)
-  const [socket, setSocket] = useState<Socket>(io('http://localhost:8080'))
+  const [socket, setSocket] = useState<Socket>(io(server))
 
   useEffect(() => {
-    const newSocket = io('http://localhost:8080')
+    const newSocket = io(server)
 
     setSocket(newSocket)
 
