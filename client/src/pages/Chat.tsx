e9de0c4b2bc81b40079fb85934b2140db474df29
Arthur Felipe <92448606+arthurfaraujo@users.noreply.github.com>
2023-11-08 21:18:03 +0000

    fix: add a useEffect
    react tells me to do it

--- client/src/pages/Chat.tsx
+++ client/src/pages/Chat.tsx
@@ -1,14 +1,16 @@
 import {useNavigate} from 'react-router-dom'
 import Form from '../components/Chat/Form'
 import Messages from '../components/Chat/Messages'
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import { SocketContext } from '../contexts/Socket'
 
 function Chat() {
   const { user } = useContext(SocketContext)
   const navigate = useNavigate()
 
-  if (!user) navigate('/join')
+  useEffect(() => {
+    !user && navigate('/join')
+  })
 
   return (
     <div className='page'>
