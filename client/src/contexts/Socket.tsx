5d532f5f8d3dbb8f4bc3aac100a3c1608c6b7a54
Arthur Felipe <92448606+arthurfaraujo@users.noreply.github.com>
2023-11-08 21:16:08 +0000

    fix: remove useEffect
    first, the useEffect was modified to
    emit a message to the server when the
    user name changed, but this was not
    needed and second, the useEffect was
    removed because the user name event is
    emitted in the updateUser
    function

--- client/src/contexts/Socket.tsx
+++ client/src/contexts/Socket.tsx
@@ -1,9 +1,9 @@
-import { createContext, ReactNode, useEffect, useState } from 'react'
-import { io, Socket } from 'socket.io-client'
+import { createContext, ReactNode, useState } from 'react'
+import { io } from 'socket.io-client'
 import { SocketContextIfc } from '../interfaces/SocketContextIfc'
 
 const server = import.meta.env.VITE_SERVER_URL || 'http://localhost:8080'
-console.log(server)
+const socket = io(server)
 
 export const SocketContext = createContext<SocketContextIfc>(
   {} as SocketContextIfc
@@ -12,21 +12,11 @@ export const SocketContext = createContext<SocketContextIfc>(
 export const SocketProvider = ({ children }: { children: ReactNode }) => {
   const username = localStorage.getItem('user') || ''
   const [user, setUser] = useState(username)
-  const [socket, setSocket] = useState<Socket>(io(server))
-
-  useEffect(() => {
-    const newSocket = io(server)
-
-    setSocket(newSocket)
-
-    return () => {
-      newSocket.disconnect()
-    }
-  }, [user])
 
   function updateUser(user: string) {
     localStorage.setItem('user', user)
     setUser(user)
+    socket.emit('setUser', user)
   }
 
   return (
