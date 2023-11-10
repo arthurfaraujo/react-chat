42561dd671b12e2f1b15d09670218c053b261e9c
arthurfaraujo <arthurfelipe5567@gmail.com>
2023-11-10 15:38:59 -0300

    feat: add a basic style to user messagey

--- client/src/components/Chat/Message.tsx
+++ client/src/components/Chat/Message.tsx
@@ -1,8 +1,12 @@
+import { useContext } from 'react'
 import { MessageIfc } from '../../interfaces/MessageIfc'
+import { SocketContext } from '../../contexts/Socket'
 
 function Message({ message }: { message: MessageIfc }) {
+  const { user } = useContext(SocketContext)
+
   return (
-    <div>
+    <div className={message.user === user ? 'myMessage' : '' }>
       <h3>{message.user}</h3>
       <p>{message.content}</p>
     </div>
